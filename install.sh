9147756ad3e2b03f312b497730f620c2bfd267c2
khansaad1275 <khansaad1275@gmail.com>
2022-03-22 18:53:09 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+# my addons
+alias note="bash /data/data/com.termux/files/home/termux-note/note.bash"
+alias w3m="w3m google.com"
+alias rnote="cat /data/data/com.termux/files/home/storage/shared/note.txt"
+echo -n -e '\e[3;42m' "Type Note to access your note tool and type rnote to read your notes"
