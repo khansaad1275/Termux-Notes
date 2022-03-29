2b24fe29d240bc7e687133a870fd92d95a646071
khansaad1275 <khansaad1275@gmail.com>
2022-03-29 14:53:48 +0530

    installer updated

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 
 # my addons
-alias note="bash /data/data/com.termux/files/home/termux-note/note.bash"
-alias w3m="w3m google.com"
-alias rnote="cat /data/data/com.termux/files/home/storage/shared/note.txt"
-echo -n -e '\e[3;42m' "Type Note to access your note tool and type rnote to read your notes"
+echo 'alias note="bash /data/data/com.termux/files/home/Termux-Notes/note.bash"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc" 
+
+echo 'alias rnote="cat /data/data/com.termux/files/home/storage/shared/note.txt"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
+
+echo 'echo -n -e '\e[3;42m' "Type Note to access your note tool and type rnote to read your notes"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
