2abc3dfcd71e4632b003082e7f901f286f4a8c0b
khansaad1275 <khansaad1275@gmail.com>
2022-03-30 17:37:56 +0530

    almost final update

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 
 # my addons
+
+echo '#-------Termux-Notes-------' >> "/data/data/com.termux/files/usr/etc/bash.bashrc" 
+
 echo 'alias note="bash /data/data/com.termux/files/home/Termux-Notes/note.bash"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc" 
 
 echo 'alias rnote="cat /data/data/com.termux/files/home/storage/shared/note.txt"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
 
-echo 'echo -n -e '\e[3;42m' "Type Note to access your note tool and type rnote to read your notes"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
+echo 'echo -n -e "\e[3;42m" "Type" "\e[5;41m" "Note" "\e[0;42m" "to access your note tool and type" "\e[5;41m" "rnote" "\e[0;42m" "to read your notes"' >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
+
+echo '#-------»»-------¤-------««-------' >> "/data/data/com.termux/files/usr/etc/bash.bashrc" 
+
--- note.bash
+++ note.bash
@@ -1,20 +1,21 @@
-# make a instaltion file to add alias alias note="bash /data/data/com.termux/files/home/note.bash"
-#echo -e '\033[32mYOUR_STRING\033[35m' hy
+# echo -e '\033[32mYOUR_STRING\033[35m' hy
 
 clear
 
-echo "----note-----"
-echo -e '\e[3;42m' "─▄▀─▄▀     How was your day?"
-echo "──▀──▀     Hello,from the other side."
-echo "█▀▀▀▀▀█▄"
-echo "█░░░░░█─█"
-echo "▀▄▄▄▄▄▀▀"
+echo ""
+echo -e '\e[034m' "█▄░█ █▀█ ▀█▀ █▀▀ █▄▄ █▀█ █▀█ █▄▀"
+echo -e '\e[034m' "█░▀█ █▄█ ░█░ ██▄ █▄█ █▄█ █▄█ █░█"
+echo -e '\e[034m' "━━◦○◦━━ Learntermux.tech ━━◦○◦━━"
+
+
 
 echo ""
 
-echo -n -e '\e[3;42m' "Note --->" '\e[0;47m'":" '\e[3;45m'
+echo -n -e '\e[3;41m' "Note『••📝••』➤ "'\e[0;47m'":" '\e[3;45m'
 
 read note
 
+echo " " >> ~/storage/shared/note.txt
 echo "$(date) : $note" >> ~/storage/shared/note.txt
+echo " " >> ~/storage/shared/note.txt
 
